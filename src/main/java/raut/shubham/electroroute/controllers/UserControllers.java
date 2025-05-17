0243296e1e676d5de8345ec1049ad13d57e26683
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-05-18 03:22:31 +0530

    added rest controller for users

---
+++ src/main/java/raut/shubham/electroroute/controllers/UserControllers.java
@@ -0,0 +1,42 @@
+package raut.shubham.electroroute.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import raut.shubham.electroroute.models.User;
+import raut.shubham.electroroute.services.UserService;
+
+import java.util.Optional;
+
+@RestController
+public class UserControllers {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World!!";
+    }
+
+    @PostMapping("/user")
+    public String saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User user) {
+        Optional<User> userOptional = userService.findById(id);
+        if(userOptional.isEmpty()){
+            return new ResponseEntity<>("User Not found",HttpStatus.NOT_FOUND);
+        }else {
+            User dbUser = userOptional.get();
+            if(user.getBrand()!= null) dbUser.setBrand(user.getBrand());
+            if(user.getModel()!= null) dbUser.setModel(user.getModel());
+            userService.saveUser(dbUser);
+            return new ResponseEntity<>("User Updated", HttpStatus.OK);
+        }
+    }
+
+}
