4597f52b0b4486cec4a88696775b2932f1e9109b
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-05-18 03:08:07 +0530

    initial

---
+++ src/main/java/raut/shubham/electroroute/ElectroRouteApplication.java
@@ -0,0 +1,13 @@
+package raut.shubham.electroroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/raut/shubham/electroroute/ElectroRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package raut.shubham.electroroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
