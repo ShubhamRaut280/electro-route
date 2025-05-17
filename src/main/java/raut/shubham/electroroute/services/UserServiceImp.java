1311793fab77c1bcf29eebfc6c18ecb6f9da6cf3
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2025-05-18 03:18:46 +0530

    implementation of user service

---
+++ src/main/java/raut/shubham/electroroute/services/UserServiceImp.java
@@ -0,0 +1,47 @@
+package raut.shubham.electroroute.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import raut.shubham.electroroute.models.User;
+import raut.shubham.electroroute.payload.SignInRequest;
+import raut.shubham.electroroute.payload.SignupRequest;
+import raut.shubham.electroroute.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public String saveUser(User user) {
+        return userRepository.save(user).getUserId();
+    }
+
+    @Override
+    public String registerUser(SignupRequest signupRequest) {
+        User user = new User();
+        user.setEmail(signupRequest.getEmail());
+        user.setFirstName(signupRequest.getFirstName());
+        user.setLastName(signupRequest.getLastName());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(user);
+        return user.getUserId();
+    }
+
+    @Override
+    public Boolean verifyUser(User user, SignInRequest signInRequest) {
+        return passwordEncoder.matches(signInRequest.getPassword(),user.getPassword());
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return userRepository.findById(id);
+    }
+}
